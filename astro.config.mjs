91e485792abc6328e1c4386c699e94177f61bcf7
lorenzo mariani <Risik@MacBook-Air-di-Lorenzo.local>
2025-06-06 14:21:02 +0200

    login unito5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import netlify from '@astrojs/netlify';
 // https://astro.build/config
-export default defineConfig({});
+
+
+
+export default defineConfig({
+  site: 'https://your-site-name.netlify.app', // Add your production URL here
+  adapter: netlify(),
+  });
